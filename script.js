c31967dd3f767b85995bd1c1ae053b8f7bd88e1d
cbugtai <cbugtai@academic.rrc.ca>
2024-10-02 10:41:39 -0500

    question 3 and 4 functions

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let gryffindor = 0;
-let hufflepuff = 0;
-let ravenclaw = 0;
-let slytherin = 0;
+var gryffindor = 0;
+var hufflepuff = 0;
+var ravenclaw = 0;
+var slytherin = 0;
 
+let question = document.getElementById("question")
+let choices = document.getElementById("Choices")
+let button1 = document.getElementById("button1")
+let button2 = document.getElementById("button2")
+let button3 = document.getElementById("button3")
+let button4 = document.getElementById("button4")
+let choice1 = document.getElementById("choice1")
+let choice2 = document.getElementById("choice2")
+let choice3 = document.getElementById("choice3")
+let choice4 = document.getElementById("choice4")
 
 
 function question1(){
@@ -31,11 +41,11 @@ function question1(){
 }
 
 function question2(){
-    question.textContent = ""
-    choice1.textContent = ""
-    choice2.textContent = ""
-    choice3.textContent = ""
-    choice4.textContent = ""
+    question.textContent = "What do you value most?"
+    choice1.textContent = "Bravery and courage."
+    choice2.textContent = "Loyalty and friendship."
+    choice3.textContent = "Knowledge and wisdom."
+    choice4.textContent = "Ambition and success."
 
     button1.addEventListener("click", () => {
         question3();
@@ -51,6 +61,56 @@ function question2(){
     })
     button4.addEventListener("click", () => {
         question3();
+        slytherin + 1;
+    })
+}
+
+function question3(){
+    question.textContent = "In a group project, what role do you usually take?"
+    choice1.textContent = "The leader who directs the team."
+    choice2.textContent = "The supporter who encourages others."
+    choice3.textContent = "The researcher who gathers information."
+    choice4.textContent = "The strategist who plans the best approach"
+
+    button1.addEventListener("click", () => {
+        question4();
+        gryffindor + 1;
+    })
+    button2.addEventListener("click", () => {
+        question4();
+        hufflepuff + 1;
+    })
+    button3.addEventListener("click", () => {
+        question4();
+        ravenclaw + 1;
+    })
+    button4.addEventListener("click", () => {
+        question4();
+        slytherin + 1;
+    })
+}
+
+function question4(){
+    question.textContent = ""
+    choice1.textContent = ""
+    choice2.textContent = ""
+    choice3.textContent = ""
+    choice4.textContent = ""
+
+    button1.addEventListener("click", () => {
+        question5();
+        gryffindor + 1;
+    })
+    button2.addEventListener("click", () => {
+        question5();
+        hufflepuff + 1;
+    })
+    button3.addEventListener("click", () => {
+        question5();
+        ravenclaw + 1;
+    })
+    button4.addEventListener("click", () => {
+        question5();
         slytherin + 1;
     })
 }
